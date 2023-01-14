69cb1afdecbd56eb90b982e7321814b6d6f876e3
Thiago Mota <thiagomota99@outlook.com>
2023-01-13 21:40:48 -0300

    Melhorias na organização e exibição dos métodos

--- src/br/com/thgyn/test/TesteCategoria.java
+++ src/br/com/thgyn/test/TesteCategoria.java
@@ -7,16 +7,16 @@ import br.com.thgyn.dao.CategoriaDAO;
 import br.com.thgyn.modelo.entidades.Categoria;
 import br.com.thgyn.modelo.services.CategoriaService;
 import br.com.thgyn.validadores.AdicaoCategoria;
-import br.com.thgyn.validadores.AtualizarCategoria;
+import br.com.thgyn.validadores.VerificarCategoria;
 
 public class TesteCategoria {
-	
+
 	private static CategoriaService categoriaService = new CategoriaService(new CategoriaDAO());
 	private static Scanner scanner = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
 		int opcao = 0;
-		while(opcao != 5) {
+		while (opcao != 5) {
 			System.out.println("=====================CATEGORIAS====================");
 			System.out.println("O que deseja fazer? ");
 			System.out.println("1 - Adicionar");
@@ -27,8 +27,8 @@ public class TesteCategoria {
 			System.out.print("R: ");
 			opcao = scanner.nextInt();
 			scanner.nextLine();
-			
-			if(opcao == 1)
+
+			if (opcao == 1)
 				TesteCategoria.adicionar();
 			else if (opcao == 2)
 				TesteCategoria.listar();
@@ -39,30 +39,35 @@ public class TesteCategoria {
 		}
 		System.out.println("Obrigado, volte sempre!!");
 	}
-	
+
 	public static void adicionar() {
 		System.out.println("===============Adicionando Nova Categoria==================");
 		System.out.print("Nome: ");
 		String nome = scanner.nextLine();
 		try {
-			categoriaService.adicionar(new Categoria(null, nome), new AdicaoCategoria());
+			categoriaService.adicionar(new Categoria(null, nome.trim()), new AdicaoCategoria());
 			System.out.println("Categoria adicionada!");
-			
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 		System.out.println("============================================================");
 	}
-	
-	public static void listar() {
+
+	public static List<Categoria> listar() {
 		System.out.println("================Listando Categorias====================");
-		for(Categoria obj : categoriaService.listar()) {
-			System.out.println("Id: " + obj.getId());
-			System.out.println("Nome: " + obj.getNome());
+		List<Categoria> categorias = categoriaService.listar();
+		if (categorias.size() > 0) {
+			for (Categoria obj : categorias) {
+				System.out.println(obj);
+			}
+		} else {
+			System.out.println("Não existe categorias.");
 		}
 		System.out.println("========================================================");
+
+		return categorias;
 	}
-	
+
 	public static void buscar() {
 		System.out.println("================Buscar Categorias====================");
 		try {
@@ -75,20 +80,25 @@ public class TesteCategoria {
 		}
 		System.out.println("========================================================");
 	}
-	
+
 	public static void atualizar() {
-		List<Categoria> categorias = categoriaService.listar();
-		int indice = 1;
-		System.out.println("================Atualizar Categorias====================");
-		for (Categoria categoria : categorias) {
-			System.out.println("Categoria " + indice + ": " + categoria);
+		Integer id = null;
+		String nome = null;
+
+		if (TesteCategoria.listar().size() > 0) {
+			System.out.println("================Atualizar Categorias====================");
+			System.out.println("Qual de deseja atualizar? ");
+			System.out.print("Digite o Id: ");
+			id = scanner.nextInt();
+			System.out.println("Nome: ");
+			scanner.nextLine();
+			nome = scanner.nextLine();
+			
+			try {
+				categoriaService.atualizar(new Categoria(id, nome), new VerificarCategoria(categoriaService));
+			} catch (Exception e) {
+				System.out.println("Erro: " + e.getMessage());
+			}
 		}
-		System.out.print("Qual deseja atualizar: ");
-		indice = scanner.nextInt();
-		Categoria obj = categorias.get(indice - 1);
-		System.out.println("Nome: ");
-		scanner.nextLine();
-		obj.setNome(scanner.nextLine());
-		categoriaService.atualizar(obj, new AtualizarCategoria());
 	}
 }
