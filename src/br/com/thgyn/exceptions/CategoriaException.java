d2961de05877d2d425f424a7a1eade10753f85fc
Thiago Mota <thiagomota99@outlook.com>
2023-01-28 10:30:28 -0300

    Adicionando serialVersionUID padrão.

--- src/br/com/thgyn/exceptions/CategoriaException.java
+++ src/br/com/thgyn/exceptions/CategoriaException.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class CategoriaException extends RuntimeException {
 	
+	private static final long serialVersionUID = 1L;
 	private List<String> erros = new ArrayList<String>();
 	
 	public CategoriaException(String msg) {
